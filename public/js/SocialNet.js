e9f4790841958efd4c8f1e01a8e75a652b7c0332
Neil Weintraut <neil.weintraut@gmail.com>
2013-04-26 12:44:15 -0400

    All loading through Require. Note do not have .js on end of files such as jQuery via Google CDN.

---
+++ public/js/SocialNet.js
@@ -0,0 +1,1 @@
+console.log("SocialNet.js placeholder");
--- public/js/boot.js
+++ public/js/boot.js
@@ -1,1 +1,18 @@
-console.log("No code yet");
+require.config({
+    appDir: ".",
+    baseUrl: "js",
+    paths: {
+     jQuery: '//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min',
+     Underscore: 'libs/underscore',
+     Backbone: 'libs/backbone',
+     text: 'libs/text',
+     templates: '../templates'
+    },
+    shim: {
+        'Backbone': ['Underscore', 'jQuery'],
+        'SocialNet': ['Backbone']
+    }
+});
+require(['SocialNet'], function(SocialNet){
+   SocialNet.initialize(); 
+});
